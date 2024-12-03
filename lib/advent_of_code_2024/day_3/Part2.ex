3c946486d904b70edd675810bcb1fb232a24feb4
Jarl André Hübenthal <jarlah@protonmail.com>
2024-12-03 09:06:10 +0100

    use pattern matching

--- lib/advent_of_code_2024/day_3/Part2.ex
+++ lib/advent_of_code_2024/day_3/Part2.ex
@@ -4,31 +4,23 @@ defmodule AdventOfCode2024.Day3.Part2.Solution do
   def solution do
     input()
     |> String.to_charlist()
-    |> List.foldl({:enabled, "", 0}, fn
-      char, {:disabled, str, sum} ->
-        cond do
-          String.match?(str, ~r/.*do\(\)$/) ->
-            {:enabled, "#{<<char>>}", sum}
-          true ->
-            {:disabled, "#{str}#{<<char>>}", sum}
-        end
-      char, {:enabled, str, sum} ->
-        cond do
-          String.match?(str, ~r/.*don't\(\)$/) ->
-            {:disabled, "#{str}#{<<char>>}", sum}
-          String.match?(str, ~r/.*mul\((\d{1,3}),(\d{1,3})\)/) ->
-            mulSum = Regex.scan(~r/.*mul\((\d{1,3}),(\d{1,3})\)/, str)
-            |> Enum.map(&(get_int_at(&1, 1) * get_int_at(&1, 2)))
-            |> List.first()
-            {:enabled, "#{<<char>>}", mulSum + sum}
-          true ->
-            {:enabled, "#{str}#{<<char>>}", sum}
-        end
-    end)
+    |> List.foldl({:enabled, "", 0}, &process_char/2)
     |> then(&(Tuple.to_list(&1) |> List.last()))
   end
 
-  defp get_int_at(str, i) do
-    str |> Enum.at(i) |> String.to_integer()
+  defp process_char(char, {:disabled, str, sum}) do
+    if String.ends_with?(str, "do()"), do: {:enabled, <<char>>, sum}, else: {:disabled, str <> <<char>>, sum}
+  end
+
+  defp process_char(char, {:enabled, str, sum}) do
+    cond do
+      String.ends_with?(str, "don't()") ->
+        {:disabled, str <> <<char>>, sum}
+      String.match?(str, ~r/mul\(\d{1,3},\d{1,3}\)$/) ->
+        [_, a, b] = Regex.run(~r/mul\((\d{1,3}),(\d{1,3})\)$/, str)
+        {:enabled, <<char>>, sum + String.to_integer(a) * String.to_integer(b)}
+      true ->
+        {:enabled, str <> <<char>>, sum}
+    end
   end
 end
