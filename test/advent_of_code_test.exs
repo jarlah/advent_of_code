c97a3db1af20266da59fff8304c662ebd5e1881b
Jarl André Hübenthal <jarlah@protonmail.com>
2024-12-03 12:31:01 +0100

    let tests last forever

--- test/advent_of_code_test.exs
+++ test/advent_of_code_test.exs
@@ -1,11 +1,12 @@
 defmodule AdventOfCode2024Test do
   use ExUnit.Case
 
+  years = 2024..2080
   days = 1..25
   modules = ["Input", "Part1.Solution", "Part2.Solution"]
 
-  for day <- days, module <- modules do
-    doctest_module = :"Elixir.AdventOfCode2024.Day#{day}.#{module}"
+  for year <- years, day <- days, module <- modules do
+    doctest_module = :"Elixir.AdventOfCode#{year}.Day#{day}.#{module}"
     if Code.ensure_loaded?(doctest_module) do
       doctest doctest_module
     end
