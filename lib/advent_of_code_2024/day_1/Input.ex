c80bf0c33216006ab0e657ceb05bfe646cc4596d
Jarl André Hübenthal <jarlah@protonmail.com>
2024-12-03 07:07:11 +0100

    refactor

--- lib/advent_of_code_2024/day_1/Input.ex
+++ lib/advent_of_code_2024/day_1/Input.ex
@@ -3,12 +3,23 @@ defmodule AdventOfCode2024.Day1.Input do
     input_lines()
     |> Enum.map(fn line ->
       line
-      |> String.split(~r"\s+")
+      |> String.split(~r/\s+/)
       |> Enum.map(&String.to_integer/1)
     end)
     |> Enum.map(&List.to_tuple/1)
     |> Enum.unzip()
     |> Tuple.to_list()
+  end
+
+  def test_input do
+    [
+      [7, 6, 4, 2, 1],
+      [1, 2, 7, 8, 9],
+      [9, 7, 6, 2, 1],
+      [1, 3, 2, 4, 5],
+      [8, 6, 4, 4, 1],
+      [1, 3, 6, 7, 9]
+    ]
   end
 
   def input_lines do
--- lib/advent_of_code_2024/day_2/Input.ex
+++ lib/advent_of_code_2024/day_2/Input.ex
@@ -8,15 +8,15 @@ defmodule AdventOfCode2024.Day2.Input do
     end)
   end
 
-  def fake do
-    """
-      7 6 4 2 1
-      1 2 7 8 9
-      9 7 6 2 1
-      1 3 2 4 5
-      8 6 4 4 1
-      1 3 6 7 9
-    """
+  def test_input do
+    [
+      [7, 6, 4, 2, 1],
+      [1, 2, 7, 8, 9],
+      [9, 7, 6, 2, 1],
+      [1, 3, 2, 4, 5],
+      [8, 6, 4, 4, 1],
+      [1, 3, 6, 7, 9]
+    ]
   end
 
   def input_lines do
