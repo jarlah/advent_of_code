1ed186d6b1942261644b100306370b08ae5f1c43
Jarl André Hübenthal <jarlah@protonmail.com>
2024-12-03 14:48:19 +0100

    prepare day4

---
+++ lib/2024/day_4/Input.ex
@@ -0,0 +1,13 @@
+defmodule AdventOfCode2024.Day4.Input do
+
+  @doc ~S"""
+  ## Examples
+
+      iex> AdventOfCode2024.Day4.Input.input()
+      "TBD"
+
+  """
+  def input do
+    File.read!(Path.join(__DIR__, "input.txt"))
+  end
+end
---
+++ lib/2024/day_4/Part1.ex
@@ -0,0 +1,12 @@
+defmodule AdventOfCode2024.Day4.Part1.Solution do
+
+  @doc ~S"""
+  ## Examples
+
+      iex> AdventOfCode2024.Day4.Part1.Solution.solution()
+      0
+
+  """
+  def solution, do: 0
+
+end
