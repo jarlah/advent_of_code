f468f6fac2b84425fa6a1d02900ddcbbc756cc16
Jarl André Hübenthal <jarlah@protonmail.com>
2024-12-06 22:23:36 +0100

    add progress

--- lib/2024/day_6/Part2.ex
+++ lib/2024/day_6/Part2.ex
@@ -17,30 +17,44 @@ defmodule AOC2024.Day6.Part2.Solution do
   def solution(input) do
     map = TileParser.parse(input)
 
-    map
-    |> Enum.with_index()
-    |> Enum.filter(fn {tile, _index} -> not (tile.is_guard or tile.is_obstacle) end)
-    |> Task.async_stream(
-      fn {tile, index} ->
-        case TileTraverser.traverse_map(
-              List.replace_at(map, index, %Tile{tile | is_obstruction: true}),
-              Enum.find(map, &Tile.is_guard/1)
-            ) do
-          {:cycling, _} ->
-            IO.inspect("Index #{index} is cycling")
-            1
-          _ ->
-            IO.inspect("OK")
-            0
-        end
-      end,
-      max_concurrency: 10, # Limit the number of concurrent tasks
-      timeout: 3_000_000_000     # Optional: Set a timeout for each task
-    )
-    |> Enum.map(fn {:ok, result} -> result end)
-    |> Enum.sum()
-  end
+    {:ok, progress_agent} = Agent.start_link(fn -> 0 end)
+    total_tasks = Enum.count(map, fn tile -> not (tile.is_guard or tile.is_obstacle) end)
 
+    result =
+      map
+      |> Enum.with_index()
+      |> Enum.filter(fn {tile, _index} -> not (tile.is_guard or tile.is_obstacle) end)
+      |> Task.async_stream(
+        fn {tile, index} ->
+          case TileTraverser.traverse_map(
+                 List.replace_at(map, index, %Tile{tile | is_obstruction: true}),
+                 Enum.find(map, &Tile.is_guard/1)
+               ) do
+            {:cycling, _} ->
+              IO.inspect("Index #{index} is cycling")
+              1
+
+            _ ->
+              IO.inspect("OK")
+              0
+          end
+          |> tap(fn _ ->
+            Agent.update(progress_agent, &(&1 + 1))
+            completed = Agent.get(progress_agent, & &1)
+            percent_done = Float.round(completed / total_tasks * 100, 2)
+            IO.inspect("#{percent_done}% completed")
+          end)
+        end,
+        max_concurrency: 10,
+        timeout: 3_000_000_000
+      )
+      |> Enum.map(fn {:ok, result} -> result end)
+      |> Enum.sum()
+
+    Agent.stop(progress_agent)
+
+    result
+  end
 
   @doc ~S"""
   ## Examples
