f79aede8cf4380b1a687cb94a4a8105c78e48dca
Jarl André Hübenthal <jarlah@protonmail.com>
2024-12-06 19:50:27 +0100

    prepare for part2

--- lib/2024/day_6/Part1.ex
+++ lib/2024/day_6/Part1.ex
@@ -2,6 +2,7 @@ defmodule AOC2024.Day6.Part1.Solution do
   alias AOC2024.Day6.Tile
   alias AOC2024.Day6.TileParser
   alias AOC2024.Day6.TileFormatter
+  alias AOC2024.Day6.TileTraverser
 
   @doc ~S"""
   ## Examples
@@ -16,7 +17,7 @@ defmodule AOC2024.Day6.Part1.Solution do
   def solution(input, print_tiles \\ false) do
     map = TileParser.parse(input)
 
-    map = traverse_map(map, Enum.find(map, &Tile.is_guard/1))
+    map = TileTraverser.traverse_map(map, Enum.find(map, &Tile.is_guard/1))
 
     if print_tiles,
       do: map |> TileFormatter.print_grid(hd(input) |> String.length()),
@@ -24,34 +25,4 @@ defmodule AOC2024.Day6.Part1.Solution do
 
     Enum.count(map, fn %Tile{visited: visited} -> visited end)
   end
-
-  defp traverse_map(map, %Tile{x: guard_x, y: guard_y, direction: direction} = guard)
-       when is_list(map) and is_atom(direction) do
-    guard_index = map |> find_index_by_coord(guard_x, guard_y)
-    new_guard = Tile.move(guard)
-    new_guard_idx = map |> find_index_by_coord(new_guard.x, new_guard.y)
-
-    if new_guard_idx && new_guard.y < length(map) && new_guard.y >= 0 do
-      if map |> Enum.at(new_guard_idx, %Tile{}) |> Tile.is_obstacle() do
-        guard = Tile.turn(guard)
-
-        traverse_map(
-          cross_out(map, guard_index),
-          Tile.move(guard)
-        )
-      else
-        traverse_map(cross_out(map, guard_index), new_guard)
-      end
-    else
-      cross_out(map, guard_index)
-    end
-  end
-
-  defp cross_out(map, index) do
-    tile = Enum.at(map, index)
-    map |> List.replace_at(index, Tile.visit(tile))
-  end
-
-  defp find_index_by_coord(map, target_x, target_y),
-    do: map |> Enum.find_index(fn %Tile{x: x, y: y} -> x == target_x && y == target_y end)
 end
