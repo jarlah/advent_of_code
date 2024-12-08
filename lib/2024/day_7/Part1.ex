94ba7e4f73d61de4ca872cf01c2d0f1c7b87212c
Jarl André Hübenthal <jarlah@protonmail.com>
2024-12-08 13:32:53 +0100

    da7 part1 is fast enough now

--- lib/2024/day_7/Part1.ex
+++ lib/2024/day_7/Part1.ex
@@ -4,8 +4,8 @@ defmodule AOC2024.Day7.Part1.Solution do
   @doc ~S"""
   ## Examples
 
-      #iex> AOC2024.Day7.Part1.Solution.solution(AOC2024.Day7.Input.input(), AOC2024.Day7.Input.normal_operators)
-      #945512582195
+      iex> AOC2024.Day7.Part1.Solution.solution(AOC2024.Day7.Input.input(), AOC2024.Day7.Input.normal_operators)
+      945512582195
 
       iex> AOC2024.Day7.Part1.Solution.solution(AOC2024.Day7.Input.test_input(), AOC2024.Day7.Input.normal_operators)
       3749
