aa4613ed119dd69827ed316d175a30e8de9de2fe
Jarl André Hübenthal <jarlah@protonmail.com>
2024-12-09 15:20:57 +0100

    day9 part1

--- lib/2024/day_9/Input.ex
+++ lib/2024/day_9/Input.ex
@@ -1,8 +1,7 @@
 defmodule AOC2024.Day9.Input do
   def input do
     File.read!(Path.join(__DIR__, "input.txt"))
-    |> String.split("\n")
-    |> Enum.map(&String.trim/1)
-    |> Enum.reject(&(&1 == ""))
   end
+
+  def test_input, do: "2333133121414131402"
 end
--- lib/2024/day_9/Part1.ex
+++ lib/2024/day_9/Part1.ex
@@ -2,9 +2,35 @@ defmodule AOC2024.Day9.Part1.Solution do
   @doc ~S"""
   ## Examples
 
-      iex> AOC2024.Day9.Part1.Solution.solution(AOC2024.Day9.Input.input())
-      0
+      iex> AOC2024.Day9.Part1.Solution.solution(AOC2024.Day9.Input.test_input())
+      "00...111...2...333.44.5555.6666.777.888899"
 
   """
-  def solution(_input), do: 0
+  def solution(input) do
+    input
+    |> String.to_charlist()
+    |> Enum.map(&String.to_integer(<<&1>>))
+    |> parse_raw_format()
+    |> tap(&IO.inspect(Enum.join(&1, ""), limit: :infinity))
+    |> Enum.join("")
+  end
+
+  defp parse_raw_format(list, acc \\ [], block_id \\ 0)
+  defp parse_raw_format([], acc, _block_id), do: acc
+
+  defp parse_raw_format([n1, n2 | tail], acc, block_id) do
+    parse_raw_format(
+      tail,
+      acc ++ Enum.map(1..n1, fn _ -> "#{block_id}" end) ++ Enum.map(1..n2, fn _ -> "." end),
+      block_id + 1
+    )
+  end
+
+  defp parse_raw_format([n1 | tail], acc, block_id) do
+    parse_raw_format(
+      tail,
+      acc ++ Enum.map(1..n1, fn _ -> "#{block_id}" end),
+      block_id + 1
+    )
+  end
 end
