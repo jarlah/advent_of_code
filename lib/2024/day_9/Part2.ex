d15b8030f56f7baf13db2035d168f26569a3c5e6
Jarl André Hübenthal <jarlah@protonmail.com>
2024-12-10 17:56:29 +0100

    refactor

--- lib/2024/day_9/Part2.ex
+++ lib/2024/day_9/Part2.ex
@@ -17,7 +17,7 @@ defmodule AOC2024.Day9.Part2.Solution do
       |> Enum.map(&String.to_integer(<<&1>>))
       |> Enum.chunk_every(2, 2)
       |> Enum.with_index()
-      |> Enum.reduce({%{}, [], 0}, &process_chunk(&1, &2))
+      |> Enum.reduce({%{}, %{}, 0}, &process_chunk(&1, &2))
 
     get_checksum(blocks, free_ranges, 0, Enum.max(blocks))
   end
@@ -25,25 +25,27 @@ defmodule AOC2024.Day9.Part2.Solution do
   defp get_checksum(_blocks, _free_ranges, checksum, nil), do: checksum
 
   defp get_checksum(blocks, free_ranges, checksum, {block_id, {block_range, block_size}}) do
-    {left_ranges, right_ranges} =
-      Enum.split_while(
-        free_ranges,
-        # IF block_size is larger than the free_range size
-        # OR if free_range first index is after block_range first index
-        # THEN we return true.
-        # ELSE we return false (then we have a free_range that is large enough AND before the block_range)
-        &(block_size > Range.size(&1) or &1.first > block_range.first)
-      )
+    maybe_free_range =
+      free_ranges
+      |> Map.filter(fn {_, free_range} ->
+        not (block_size > Range.size(free_range) or free_range.first > block_range.first)
+      end)
+      |> Enum.min(fn -> nil end)
 
     {updated_free_ranges, new_checksum} =
-      case right_ranges do
-        [fits | remaining] ->
-          {overlapping_range, remaining_free} = Range.split(fits, block_size)
-          updated_free_ranges = left_ranges ++ [remaining_free | remaining]
+      case maybe_free_range do
+        {free_range_idx, free_range} ->
+          {overlapping_range, remaining_free} = Range.split(free_range, block_size)
+
+          updated_free_ranges =
+            if Range.size(remaining_free) > 0,
+              do: free_ranges |> Map.put(free_range_idx, remaining_free),
+              else: free_ranges |> Map.delete(free_range_idx)
+
           new_checksum = checksum + block_id * Enum.sum(overlapping_range)
           {updated_free_ranges, new_checksum}
 
-        [] ->
+        nil ->
           new_checksum = checksum + block_id * Enum.sum(block_range)
           {free_ranges, new_checksum}
       end
@@ -63,22 +65,25 @@ defmodule AOC2024.Day9.Part2.Solution do
        ) do
     next_free_size = List.first(maybe_free_space, 0)
 
-    # we append a new range to free ranges if next_free_size is not zero
+    start_idx_free_space = current_count + number
+
     new_free_ranges =
-      if next_free_size > 0,
-        do:
-          free_ranges ++
-            [
-              (current_count + number)..(current_count + number + next_free_size - 1)
-            ],
-        else: free_ranges
+      if next_free_size > 0 do
+        Map.put(
+          free_ranges,
+          start_idx_free_space,
+          start_idx_free_space..(start_idx_free_space + next_free_size - 1)
+        )
+      else
+        free_ranges
+      end
 
     # we add the number since the number should repeat "number" times
     # we also add the next free space since that will amount to next free space amount of dots.
-    new_current_count = current_count + number + next_free_size
+    new_current_count = start_idx_free_space + next_free_size
 
-    block_range = current_count..(current_count + number - 1)
-    new_blocks = Map.put(blocks, next_block_id, {block_range, Range.size(block_range)})
+    block_range = current_count..(start_idx_free_space - 1)
+    new_blocks = blocks |> Map.put(next_block_id, {block_range, Range.size(block_range)})
 
     {new_blocks, new_free_ranges, new_current_count}
   end
