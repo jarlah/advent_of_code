b4b055e969c48cbc385d4c14ba686b5d3b8eccf3
Jarl André Hübenthal <jarlah@protonmail.com>
2024-12-15 09:47:13 +0100

    day 14 part 2 done

--- solutions/2024/day_14/lib/Part2.ex
+++ solutions/2024/day_14/lib/Part2.ex
@@ -1,4 +1,6 @@
 defmodule AOC2024.Day14.Part2.Solution do
+  import AOC2024.Day14.Part1.Solution
+
   @doc ~S"""
   ## Examples
 
@@ -6,5 +8,36 @@ defmodule AOC2024.Day14.Part2.Solution do
       0
 
   """
-  def solution(_input), do: 0
+  def solution(input) do
+    robots =
+      input
+      |> parse_robots()
+
+    for seconds <- Stream.iterate(1, &(&1 + 2)) do
+      robots = move_robots(robots, 101, 103, seconds)
+
+      grid_string =
+        robots
+        |> Enum.map(&elem(&1, 0))
+        |> print_grid(101, 103)
+
+      if String.contains?(grid_string, "RRRRRRRR") do
+        IO.puts(grid_string)
+        IO.puts("Found a match at #{seconds} seconds")
+        exit("found grid")
+      end
+    end
+  end
+
+  defp print_grid(robots, cols, rows) do
+    positions = robots |> MapSet.new()
+
+    for y <- 0..(rows - 1), into: "" do
+      for x <- 0..(cols - 1), into: "" do
+        if MapSet.member?(positions, {x, y}), do: "R", else: "."
+      end <>
+        "\n"
+    end <>
+      "\n"
+  end
 end
--- solutions/2024/day_14/test/doc_test.exs
+++ solutions/2024/day_14/test/doc_test.exs
@@ -1,6 +1,8 @@
 defmodule AOC2024.Day14.DocTest do
   use ExUnit.Case
 
+  @moduletag timeout: :infinity
+
   doctest AOC2024.Day14.Part1.Solution
   doctest AOC2024.Day14.Part2.Solution
 end
