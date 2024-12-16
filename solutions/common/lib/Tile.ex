a024a36aad8b553f36e6f28df931128547f82eea
Jarl André Hübenthal <jarlah@protonmail.com>
2024-12-16 22:40:38 +0100

    refactor Tile

--- solutions/common/lib/Tile.ex
+++ solutions/common/lib/Tile.ex
@@ -1,6 +1,7 @@
 defmodule Tile do
   @type type :: :robot | :box | :obstacle
   @type t() :: %__MODULE__{
+          id: any(),
           x: integer(),
           y: integer(),
           width: integer(),
@@ -9,7 +10,8 @@ defmodule Tile do
           display: String.t()
         }
 
-  defstruct x: nil,
+  defstruct id: nil,
+            x: nil,
             y: nil,
             width: 1,
             height: 1,
@@ -21,6 +23,13 @@ defmodule Tile do
   end
 
   def print_tile_map(map, options \\ []) do
+    case Keyword.get(options, :layout, :simple) do
+      :simple -> print_simple_tile_map(map, options)
+      :advanced -> print_advanced_tile_map(map, options)
+    end
+  end
+
+  defp print_simple_tile_map(map, options) do
     default_tile = Keyword.get(options, :default_tile, ".")
 
     rows = Enum.max_by(map, & &1.y).y + 1
@@ -31,6 +40,42 @@ defmodule Tile do
         for x <- 0..(cols - 1), into: [] do
           case Enum.find(map, fn tile -> tile.x == x and tile.y == y end) do
             %Tile{display: display} -> display
+            _ -> default_tile
+          end
+        end
+      end
+
+    grid
+    |> Enum.map(&Enum.join(&1, ""))
+    |> Enum.each(&IO.puts/1)
+
+    map
+  end
+
+  defp print_advanced_tile_map(map, options) do
+    default_tile = Keyword.get(options, :default_tile, ".")
+
+    max_x = Enum.max_by(map, fn tile -> tile.x + tile.width - 1 end)
+    max_y = Enum.max_by(map, fn tile -> tile.y + tile.height - 1 end)
+
+    rows = max_y.y + max_y.height
+    cols = max_x.x + max_x.width
+
+    grid =
+      for y <- 0..(rows - 1) do
+        for x <- 0..(cols - 1) do
+          tile = Enum.find(map, fn t ->
+            x >= t.x and x < t.x + t.width and
+            y >= t.y and y < t.y + t.height
+          end)
+
+          case tile do
+            %Tile{display: display, width: w, height: h} ->
+              if x >= tile.x and x < tile.x + w and y >= tile.y and y < tile.y + h do
+                display
+              else
+                default_tile
+              end
             _ -> default_tile
           end
         end
--- solutions/common/test/common_test.exs
+++ solutions/common/test/common_test.exs
@@ -1,8 +1,27 @@
 defmodule CommonTest do
   use ExUnit.Case
-  doctest Common
 
-  test "greets the world" do
-    assert Common.hello() == :world
+  test "test map" do
+    test_map = [
+      # Walls
+      %Tile{id: :wall1, x: 0, y: 0, width: 30, height: 1, type: :obstacle, display: "#"},
+      %Tile{id: :wall2, x: 0, y: 1, width: 1, height: 8, type: :obstacle, display: "#"},
+      %Tile{id: :wall3, x: 29, y: 1, width: 1, height: 8, type: :obstacle, display: "#"},
+      %Tile{id: :wall4, x: 0, y: 9, width: 30, height: 1, type: :obstacle, display: "#"},
+
+      # Boxes
+      %Tile{id: :box1, x: 5, y: 3, width: 2, height: 2, type: :box, display: "O"},
+      %Tile{id: :box2, x: 15, y: 5, width: 2, height: 2, type: :box, display: "O"},
+      %Tile{id: :box3, x: 22, y: 2, width: 2, height: 2, type: :box, display: "O"},
+
+      # Player
+      %Tile{id: :player, x: 10, y: 4, width: 1, height: 1, type: :robot, display: "P"},
+
+      # Obstacles
+      %Tile{id: :obstacle1, x: 8, y: 7, width: 3, height: 1, type: :obstacle, display: "X"},
+      %Tile{id: :obstacle2, x: 20, y: 6, width: 1, height: 2, type: :obstacle, display: "Y"},
+    ]
+
+    Tile.print_tile_map(test_map)
   end
 end
